673e32ff6a629538595863d74d4af87764dba962
pilgrimmemoirs <jpilgrim@avvo.com>
2018-03-07 21:48:41 -0800

    add router

---
+++ src/group-lookup.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css'; //customize for this component
+
+class GroupLookup extends Component {
+  
+
+  render() {
+    return (
+      <h2> Hello World! </h2>
+    );
+  }
+}
+
+export default GroupLookup;
